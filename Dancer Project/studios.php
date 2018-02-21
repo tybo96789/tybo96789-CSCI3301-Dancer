6d40745801e5663c993f6d62f3d738b17040b29d
Tyler A <tybo96789@users.noreply.github.com>
2018-02-20 21:37:42 -1000

    Inital upload of the project
    Final version of the project

---
+++ Dancer Project/studios.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title></title>
+    <link rel="stylesheet" href="./CSS/style.css">
+  <body>
+
+    <header>
+      <div class = "container"><!--Navigation Area-->
+          <div id="Basename">
+              <h1><span class="highlight">Dancer</span> Database</h1>
+          </div>
+          <nav>
+              <ul>
+                  <li><a href="studios.html" style="text-decoration:none">Studios</a></li>
+              </ul>
+          </nav>
+      </div>
+    </header>
+      <section id="waipahuimg"><!--This Section contains information about the Database-->
+          <div class="container">
+            <h1>Studio 808</h1>
+              <p>Located in the little town of Waipahu, this Dance Studio is known to bring energy and fire to the Dancefloor. </p>
+          </div>
+
+  </body>
+</html>
